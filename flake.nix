88626f17cb77d31df5eccd3e7de6f7da9355ae7f
Bobbbay <abatterysingle@gmail.com>
2021-05-12 14:47:00 -0400

    Nix: Add REPL, touchup flake

--- flake.nix
+++ flake.nix
@@ -4,31 +4,45 @@
     utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
     fenix.url = "github:nix-community/fenix";
     naersk.url = "github:nmattia/naersk";
+    deploy-rs.url = "github:serokell/deploy-rs";
   };
 
-  outputs = { self, nixpkgs, utils, fenix, naersk }:
-  with builtins;
-  utils.lib.systemFlake {
-    inherit self inputs;
+  outputs = { self, nixpkgs, utils, fenix, naersk, deploy-rs }:
+    with builtins;
+    utils.lib.systemFlake {
+      inherit self inputs;
 
-		channels.nixpkgs = { input = nixpkgs; };
+      channels.nixpkgs = {
+        input = nixpkgs;
+        overlaysBuilder = channels: [
+          (final: prev: {
+            inherit (deploy-rs.packages.${prev.system}) deploy-rs;
+          })
+        ];
+      };
 
-    defaultPackage = {
-      x86_64-linux = ((naersk.lib.x86_64-linux.override {
-        inherit (fenix.packages.x86_64-linux.minimal) cargo rustc;
-	    }).buildPackage {
-        src = ./.;
-        nativeBuildInputs = [ nixpkgs.legacyPackages.x86_64-linux.pandoc ];
-      });
+      sharedOverlays = [
+        (final: prev: {
+          inherit (deploy-rs.packages.${prev.system}) deploy-rs;
+        })
+      ];
+
+      defaultPackage = {
+        x86_64-linux = ((naersk.lib.x86_64-linux.override {
+          inherit (fenix.packages.x86_64-linux.minimal) cargo rustc;
+        }).buildPackage {
+          src = ./.;
+        });
+      };
+
+      devShellBuilder = channels:
+        with channels.nixpkgs;
+        mkShell {
+          buildInputs = [
+            pandoc
+            diesel-cli
+            nixpkgs-fmt
+          ];
+        };
     };
-
-		devShellBuilder = channels:
-		  with channels.nixpkgs;
-		  mkShell {
-		    buildInputs = [
-		      pandoc
-		      diesel-cli
-		    ];
-		  };
-  };
 }
