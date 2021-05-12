0988049004730bce4e286b7fc61b2fddc373f9d9
Bobbbay <abatterysingle@gmail.com>
2021-05-11 22:08:29 -0400

    Style: add CSS in build_posts

--- src/tools.rs
+++ src/tools.rs
@@ -6,6 +6,7 @@ use anyhow::Result;
 use std::fs::read_dir;
 
 use pandoc::OutputKind;
+use pandoc::PandocOption::{Css, Standalone};
 
 /// `compile_posts` compiles all `.md` files in /posts of the project directory into HTML files.
 pub(crate) fn build_posts() -> Result<()> {
@@ -38,6 +39,8 @@ pub(crate) fn build_posts() -> Result<()> {
         let mut pandoc = pandoc::new();
         pandoc.add_input(&post);
         pandoc.set_output(OutputKind::File(output.clone()));
+        pandoc.add_option(Css("/css/index.css".to_string()));
+        pandoc.add_option(Standalone);
         pandoc.execute().unwrap();
 
         println!("> Completed {:?} > {:?}", post, output);
